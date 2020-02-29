b789de75153986c932c74a34b303dfb259c2ec3d
Onkar Kandalgaonkar <onkark17@gmail.com>
2020-02-29 23:42:46 +0530

    Completed implementations for: #status #product_codes_for_products_with_colour #slot_number_for_product_code

--- WarehouseService/src/main/java/com/fynd/warehouse/service/WarehouseService.java
+++ WarehouseService/src/main/java/com/fynd/warehouse/service/WarehouseService.java
@@ -1,6 +1,7 @@
 package com.fynd.warehouse.service;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -20,11 +21,14 @@ public class WarehouseService {
 	}
 
 	public String store(Long productCode, String color) {
+		
+		if(warehouse.getCapacity()==Arrays.asList(warehouse.getRacks()).stream().filter(rack-> null!=rack).count())
+			return "Warehouse is full";
+		
 		Product product = new Product(productCode, color);
 		Rack rack = new Rack(product);
-		
 		warehouse.getRacks()[WarehouseService.index] = rack;
-		String response="Allocated slot number : " + WarehouseService.index;
+		String response="Allocated slot number : " + (WarehouseService.index+1);
 		
 		for(int p=0;p<warehouse.getRacks().length;p++) {
 			if(warehouse.getRacks()[p]==null) {
@@ -37,14 +41,35 @@ public class WarehouseService {
 	}
 
 	public String sell(Integer slotNo) {
-		if(slotNo<warehouse.getCapacity()) {
-			Rack rack = warehouse.getRacks()[slotNo];
-			if(rack!=null) warehouse.getRacks()[slotNo]=null;
+		if(slotNo-1<warehouse.getCapacity()) {
+			Rack rack = warehouse.getRacks()[slotNo-1];
+			if(rack!=null) warehouse.getRacks()[slotNo-1]=null;
 			System.out.println(Arrays.toString(warehouse.getRacks()));
-			return "Slot number "+slotNo+" is free";
+			
+			return "Slot number "+(slotNo)+" is free";
 		}
 		System.out.println(Arrays.toString(warehouse.getRacks()));
-		return "Invalid slot : "+slotNo;
+		
+		return "Invalid slot : "+(slotNo);
+	}
+
+	public void status() {
+		Arrays.asList(warehouse.getRacks()).stream().filter(rack -> null!=rack).forEach(rack -> System.out.println(Arrays.asList(warehouse.getRacks()).indexOf(rack)+"\t"+rack.getProduct().getProductCode()+"\t"+rack.getProduct().getColor()));
+	}
+	
+	public String product_codes_for_products_with_colour(String color) {
+		return Arrays.asList(warehouse.getRacks()).stream().filter(rack -> rack.getProduct().getColor().equals(color)).map(rack->rack.getProduct().getProductCode().toString()).collect(Collectors.joining(" , "));
+	}
+
+	public String slot_number_for_product_code(Long productCode) {
+		Rack[] racks = warehouse.getRacks();
+		for(int p=0;p<racks.length;p++) {
+			if(racks[p]!=null && racks[p].getProduct().getProductCode().equals(productCode)) {
+				 return String.valueOf(p+1);
+			}
+		}
+		
+		return "Not found";
 	}
 
 }
--- WarehouseService/src/main/java/com/fynd/warehouse/shell/WarehouseCommands.java
+++ WarehouseService/src/main/java/com/fynd/warehouse/shell/WarehouseCommands.java
@@ -9,30 +9,44 @@ import com.fynd.warehouse.service.WarehouseService;
 
 @ShellComponent
 public class WarehouseCommands {
-	
-	private WarehouseService warehouseService; 
-	
+
+	private WarehouseService warehouseService;
+
 	@Autowired
 	public WarehouseCommands(WarehouseService warehouseService) {
-		this.warehouseService=warehouseService;
+		this.warehouseService = warehouseService;
 	}
 
-    @ShellMethod("Create a warehouse.")
-    public void warehouse(Integer capacity) {
-    	Warehouse warehouse = warehouseService.create(capacity);
-    	System.out.println("Created a warehouse with "+warehouse.getCapacity()+" slots");
-    }
-    
-    @ShellMethod("Create a warehouse.")
-    public void store(Long productId,String color) {
-    	String response = warehouseService.store(productId,color);
-    	System.out.println(response);
-    }
-    
-    
-    @ShellMethod("Sell product.")
-    public void sell(Integer slotNo) {
-    	String response = warehouseService.sell(slotNo);
-    	System.out.println(response);
-    }
+	@ShellMethod("Create a warehouse.")
+	public void warehouse(Integer capacity) {
+		Warehouse warehouse = warehouseService.create(capacity);
+		System.out.println("Created a warehouse with " + warehouse.getCapacity() + " slots");
+	}
+
+	@ShellMethod("Create a warehouse.")
+	public void store(Long productId, String color) {
+		String response = warehouseService.store(productId, color);
+		System.out.println(response);
+	}
+
+	@ShellMethod("Sell product.")
+	public void sell(Integer slotNo) {
+		String response = warehouseService.sell(slotNo);
+		System.out.println(response);
+	}
+
+	@ShellMethod("Status.")
+	public void status() {
+		warehouseService.status();
+	}
+
+	@ShellMethod("product_codes_for_products_with_colour.")
+	public void product_codes_for_products_with_colour(String color) {
+		System.out.println(warehouseService.product_codes_for_products_with_colour(color));
+	}
+	
+	@ShellMethod("slot_number_for_product_code.")
+	public void slot_number_for_product_code(Long productCode) {
+		System.out.println(warehouseService.slot_number_for_product_code(productCode));
+	}
 }
